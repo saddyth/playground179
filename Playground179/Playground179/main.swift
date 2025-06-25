122fbb686ae514212531310da28540c2bd103ef8
saddyth <vesninap@mail.ru>
2025-06-25 19:33:44 +0300

    Выполнена 2 подзадача

--- Playground179/Playground179/main.swift
+++ Playground179/Playground179/main.swift
@@ -55,3 +55,112 @@ func isEven(number: Int) -> Bool {
 }
 let isEvenResult = isEven(number: 6)
 print(isEvenResult)
+
+
+//Task 2.1
+//Напишите функцию calculate(a:b:operation:), которая принимает два числа и операцию (+, -, *, /) и возвращает результат вычисления.Обработайте случай деления на ноль.
+func calculate(a: Int, b: Int, operation: String) -> Int? {
+    guard b != 0 else {
+        return nil
+    }
+    switch operation {
+    case "+":
+        return a + b
+    case "-":
+        return a - b
+    case "*":
+        return a * b
+    case "/":
+        return a / b
+    default:
+        return nil
+    }
+}
+if let calcResult = calculate(a: 12, b: 0, operation: "/") {
+    print(calcResult)
+} else {
+    print("На 0 делить нельзя")
+}
+
+//Task 2.2
+//Напишите функцию findMax(numbers:), которая принимает массив чисел и возвращает максимальное значение.
+//Если массив пустой, функция должна вернуть nil.
+
+func findMax(numbers: [Int]) -> Int? {
+    guard !numbers.isEmpty else {
+        return nil
+    }
+    var maxValue = numbers[0]
+    for number in numbers {
+        if number > maxValue {
+            maxValue = number
+        }
+    }
+    return maxValue
+}
+if let maxResult = findMax(numbers: [-10,-5,-2,-6,-1,-7,-5]) {
+    print(maxResult)
+} else{
+    print("Массив пустой")
+}
+
+//Task 2.3
+// Генерация таблицы умножения
+//Напишите функцию multiplicationTable(for:), которая принимает число и выводит таблицу умножения для этого числа.
+
+func multiplicationTable(number: Int) -> [Int]? {
+    guard number != 0 else {
+        return nil
+    }
+    var multArr = [Int]()
+    for i in 0...number {
+        multArr.append(number * i)
+    }
+    return multArr
+}
+if let multResult = multiplicationTable(number: 0) {
+    for (i, number) in multResult.enumerated() {
+        print("\(multResult[1]) x \(multResult[i]) = \(number)")
+    }
+} else {
+    print("Введен 0")
+}
+
+//Task 2.4
+//Напишите функцию reverseString(_:), которая принимает строку и возвращает ее в обратном порядке.
+//Например, для строки "hello" результат должен быть
+
+func reverseString(word: String) -> String? {
+    guard !word.isEmpty else{
+        return nil
+    }
+    let reversedWord = String(word.reversed())
+    return reversedWord
+}
+
+if let reverseResul = reverseString(word: "Hello") {
+    print(reverseResul)
+} else {
+    print("Строка пустая")
+}
+
+//Task 2.5
+//Напишите функцию isPalindrome(_:), которая проверяет, является ли строка палиндромом (читается одинаково слева направо и справа налево).
+
+func isPalindrome(word: String) -> Bool? {
+    guard !word.isEmpty else{
+        return nil
+    }
+    if word.lowercased() == String(word.lowercased().reversed()) {
+        return true
+    } else {
+        return false
+    }
+}
+if let palindromeResult = isPalindrome(word: "Radar") {
+    print(palindromeResult)
+} else {
+    print("Строка пустая")
+}
+
+
